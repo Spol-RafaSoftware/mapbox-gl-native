713e237632337c75fbd1b7539abf15ef146ed998
Ivo van Dongen <info@ivovandongen.nl>
2017-06-28 16:32:00 -0700

    [qt] render frontend implementation

--- platform/qt/src/qmapboxgl_p.hpp
+++ platform/qt/src/qmapboxgl_p.hpp
@@ -22,12 +22,12 @@ public:
 
     mbgl::Size framebufferSize() const;
 
+
     // mbgl::View implementation.
     void bind() final;
 
     // mbgl::Backend implementation.
     void updateAssumedState() final;
-    void invalidate() final;
     void activate() final {}
     void deactivate() final {}
 
@@ -63,9 +63,14 @@ private:
 
 public slots:
     void connectionEstablished();
+    void invalidate();
+    void render();
 
 signals:
     void needsRendering();
     void mapChanged(QMapboxGL::MapChange);
     void copyrightsChanged(const QString &copyrightsHtml);
+    
+    // Internal signal to start rendering
+    void renderRequested();
 };
